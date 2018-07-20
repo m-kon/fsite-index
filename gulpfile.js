936c8be935522f2f3e9162b9d3485ee0ac0aa3d4
m-kon <mark.konstantinovi4@gmail.com>
2018-07-20 13:56:12 +0300

    add some dev packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,30 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('autoprefixer');
 const exec = require('child_process').exec;
 
 gulp.task('sass', function() {
-    gulp.src('sass/*.sass')
+    return gulp.src('sass/*.sass')
         .pipe(sass())
         .pipe(gulp.dest('css/'));
 });
 
-gulp.task('watch', ['sass'], function() {
-    gulp.watch('sass/*.sass', ['sass']);
+gulp.task('autoprefixer', function () {
+    return gulp.src('css/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss([ autoprefixer() ]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./css'));
 });
 
-gulp.task('default', ['watch'], function() {
-  exec('live-server');
-});
+gulp.task('watch', gulp.series('sass', 'autoprefixer', function(done) {
+    gulp.watch('sass/*.sass', gulp.series('sass', 'autoprefixer'));
+    done();
+}));
+
+gulp.task('default', gulp.series('watch', function(done) {
+    exec('live-server');
+    done();
+}));
